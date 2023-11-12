50001dac4f13b9423c7dec1686fe46c491ab873c
mattia <mattiapivi@gmail.com>
2023-11-13 00:06:16 +0100

    1) fizzbuz function done

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,16 @@ package dssc.assignment.fizzbuzz;
 public class FizzBuzz {
 
     public static void main(String... args) {
-        
+        String buzz = "buzz", fizz = "fizz", fizzbuzz = "fizzBuzz"; //initialise string variables
+        int number = Integer.parseInt(args[0]);
+        for (int i = 1; i <= number; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(fizzbuzz);
+            } else if (i % 3 == 0) {
+                System.out.println(fizz);
+            } else if (i % 5 == 0) {
+                System.out.println(buzz);
+            } else System.out.println(i);
+        }
     }
-
 }
